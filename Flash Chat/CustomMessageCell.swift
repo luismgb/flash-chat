fa5d1f7276ddded5a43e97063bcdd5fe21929a4e
luismgb <luismigelgb@gmail.com>
2018-01-24 08:45:09 -0800

    Initial commit

---
+++ Flash Chat/CustomMessageCell.swift
@@ -0,0 +1,28 @@
+//
+//  CustomMessageCell.swift
+//  Flash Chat
+//
+//  Created by Luis M Gonzalez on 1/24/18.
+//  Copyright © 2018 Luis M Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class CustomMessageCell: UITableViewCell {
+
+
+    @IBOutlet var messageBackground: UIView!
+    @IBOutlet var avatarImageView: UIImageView!
+    @IBOutlet var messageBody: UILabel!
+    @IBOutlet var senderUsername: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code goes here
+        
+        
+        
+    }
+
+
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+![Flash Chat Icon](https://dl.dropboxusercontent.com/s/x4nq6hhhr6my4so/Flash%20Chat%20Icon-60%402x.png?dl=0)
+
+# Flash Chat
+
+**CURRENTLY IN DEVELOPMENT**
+
+iOS App where users can log in and chat with other members that have signed up.
+
+## Authors
+
+Luis M Gonzalez
+
+Assets by TheLondonAppBrewery.
